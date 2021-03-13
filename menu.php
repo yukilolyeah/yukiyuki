8b2ca936aa739591238fc0e95afacaea266c8c3f
yukilolyeah <78223049+yukilolyeah@users.noreply.github.com>
2021-03-13 17:07:18 +0800

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,9 @@
+<div class="header">
+            <a href="index.html">Главная</a>
+            <span>|</span>
+            <a href="puzzle.php">Загадки</a>
+            <span>|</span>
+            <a href="guess.html">Угадайка</a>
+            <span>|</span>
+            <a href="passw-generator.html">Генератор паролей</a>
+        </div>
