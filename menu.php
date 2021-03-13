2c8b35f6057a0e21b2c192f83f6e36366b96b93b
yukilolyeah <78223049+yukilolyeah@users.noreply.github.com>
2021-03-13 17:15:46 +0800

    Delete menu.php

--- menu.php
+++
@@ -1,9 +0,0 @@
-<div class="header">
-            <a href="index.html">Главная</a>
-            <span>|</span>
-            <a href="puzzle.php">Загадки</a>
-            <span>|</span>
-            <a href="guess.html">Угадайка</a>
-            <span>|</span>
-            <a href="passw-generator.html">Генератор паролей</a>
-        </div>
